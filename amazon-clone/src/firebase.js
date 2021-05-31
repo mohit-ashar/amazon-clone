50bbf21995522ab65a6a19e32d869c22bbaccade
Mohit <mohitashar1995@gmail.com>
2021-05-31 13:44:14 +0530

    basic Amazon header layout

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBcYGz5c_ljpqKzww5YPSSHWVMf_JFFUAo",
+    authDomain: "challenge-609ba.firebaseapp.com",
+    projectId: "challenge-609ba",
+    storageBucket: "challenge-609ba.appspot.com",
+    messagingSenderId: "955288723815",
+    appId: "1:955288723815:web:4dfdc03a9763257c5c0fc2",
+    measurementId: "G-MHCFV4D97E"
+  };
